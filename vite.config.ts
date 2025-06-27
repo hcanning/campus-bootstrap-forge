51d887cbf966d2bc21f62c19877e6fd8c4bb8b53
hcanning <46687863+hcanning@users.noreply.github.com>
2025-06-27 11:58:18 -0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/campus-bootstrap-forge",
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  base: '/campus-bootstrap-forge',
+  plugins: [react(), mode === 'development' && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './campus-bootstrap-forge/src'),
     },
   },
 }));
